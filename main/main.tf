52f5e357bab42fd15e739ab786f7e9ebe528ebd1
Massimo <valentino.massimo@atos.net>
2020-04-10 18:29:14 -0400

    first commit

---
+++ main/main.tf
@@ -0,0 +1,92 @@
+provider "google" {
+  credentials = file(.../demo-onpremisis2gcp-serviceAccount.json")
+
+  project = "onpremis2gcp"
+  region  = "uscentral-3"
+  zone    = "uscentral-3a"
+}
+
+#creates data migration bucket
+resource "google_storage_bucket" "service-x-bucket" {
+  name                = "onpremis2gcp-migrated-data-service-x"
+  location            = "uscentral-3"
+  force_destroy       = "false"
+  storage_class       = "REGIONAL"
+  bucket_policy_only  = false
+  encryption {
+    default_kms_key_name = "projects/onpremis2gcp/locations/uscentral-west3/keyRings/gcp-target-key-ring-3/cryptoKeys/service-x-crypto-key"
+  }  
+  labels              = {
+    service = "service-x"
+  }
+
+  /*provisioner "local-exec" {
+    command = "echo hadoop distcp hdfs://10.156.0.2/service-x/ gs://onpremis2gcp-migrated-data-service-x/ > ./data/import.sh"
+  }*/
+}
+
+#creates support bucket
+resource "google_storage_bucket" "support-bucket" {
+  name          = "onpremis2gcp-gcp-target-data"
+  location      = "EU"
+  depends_on = [
+        google_storage_bucket.service-x-bucket,
+  ]
+}
+
+#uploads data import script in the support bucket
+resource "google_storage_bucket_object" "import-script" {
+  name   = "pull.sh"
+  source = "./data/pull.sh"
+  bucket = "onpremis2gcp-gcp-target-data"
+  depends_on = [
+        google_storage_bucket.support-bucket,
+  ]
+}
+
+#uploads empty job in the support bucket
+resource "google_storage_bucket_object" "job" {
+  name   = "empty.py"
+  source = "./data/empty.py"
+  bucket = "onpremis2gcp-gcp-target-data"
+  depends_on = [
+        google_storage_bucket.support-bucket,
+  ]
+}
+
+#Local executor that creates workflow and cluster templates for later execution
+resource "null_resource" "create-template-and-cluster" {
+  provisioner "local-exec" {
+    command = "./data/createWFTandEC.sh"
+  }
+}
+
+#Local executor that destroys workflow and cluster templates
+resource "null_resource" "delete-template" {
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "gcloud dataproc workflow-templates delete pull-cluster-template --region uscentral-west3"
+  }
+}
+
+#SPIN UP THE EPHEMERAL CLUSTER ONCE THE MIGRATION BUCKET IS CREATED
+#THE CLUSTER WILL BE AUTOMATICALLY DELETED ONCE THE DATA PULL IS COMPLETED
+resource "null_resource" "pull" {
+  provisioner "local-exec" {
+    command = "./data/executeJob.sh"
+  }
+  depends_on = [
+        google_storage_bucket_object.job,
+  ]
+}
+
+#Key rings and keys 
+/*resource "google_kms_key_ring" "gcp-target-key-ring-3" {
+  name     = "gcp-target-key-ring-3"
+  location = "uscentral-west3"
+}
+
+resource "google_kms_crypto_key" "service-x-crypto-key" {
+  name     = "service-x-crypto-key"
+  key_ring = "projects/onpremis2gcp/locations/uscentral-west3/keyRings/gcp-target-key-ring-3/cryptoKeys/service-x-crypto-key"
+}*/
