56a6926f8848577d4c422171969e3e73ba071c28
Massimo <valentino.massimo@atos.net>
2020-04-10 20:33:29 -0400

    commit adding .json

---
+++ step1-create-environments/service-y/load.sh
@@ -0,0 +1,2 @@
+hadoop distcp gs://onpremis2gcp-onpremis-data/service-x/ hdfs://on-prem-cluster-m/service-x/
+hadoop distcp gs://onpremis2gcp-onpremis-data/service-y/ hdfs://on-prem-cluster-m/service-y/F
